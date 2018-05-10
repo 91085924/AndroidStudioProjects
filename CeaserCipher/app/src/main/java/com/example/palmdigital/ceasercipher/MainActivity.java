af2f0b3a4853d12df51a2dfd2622f646501edbfb
91085924 <91085924@mvusd.net>
2018-05-10 14:28:26 -0700

    Finished android Ceaser

--- CeaserCipher/app/src/main/java/com/example/palmdigital/ceasercipher/MainActivity.java
+++ CeaserCipher/app/src/main/java/com/example/palmdigital/ceasercipher/MainActivity.java
@@ -15,7 +15,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button Encrypt = findViewById(R.id.Encrypt);
+        Encrypt.setOnClickListener(this);
         Button Decrypt = findViewById(R.id.Decrypt);
+        Decrypt.setOnClickListener(this);
     }
 
     public void onClick(View v)
@@ -23,16 +25,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         EditText InputText = findViewById(R.id.InputText);
         EditText InputShift = findViewById(R.id.InputShift);
         String StrShift = InputShift.getText().toString();
-        int shift = Integer.parseInt(StrShift);
 
 
         if (v.getId() == R.id.Encrypt)
         {
             String unencrypted = InputText.getText().toString();
+            int shift = Integer.parseInt(StrShift);
             String encrypted = Encrypt(unencrypted,shift);
             TextView tvEncrypted = findViewById(R.id.EncryptedString);
             tvEncrypted.setText(encrypted);
 
+        }
+
+        else if(v.getId() == R.id.Decrypt)
+        {
+            String encrypted = InputText.getText().toString();
+            int shift = Integer.parseInt(StrShift);
+            String decrypted = Decrypt(encrypted,shift);
+            TextView tvDecrypted = findViewById(R.id.DecryptedString);
+            tvDecrypted.setText(decrypted);
         }
     }
 
@@ -92,5 +103,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         return output;
     }
+    public static String Decrypt(String input, int shift)
+    {
+        String output = "";
+        int alphabetSize = 26;
+        char[] alphabet = new char[alphabetSize];
+        alphabet[0] = 'A';
+        alphabet[1] = 'B';
+        alphabet[2] = 'C';
+        alphabet[3] = 'D';
+        alphabet[4] = 'E';
+        alphabet[5] = 'F';
+        alphabet[6] = 'G';
+        alphabet[7] = 'H';
+        alphabet[8] = 'I';
+        alphabet[9] = 'J';
+        alphabet[10] = 'K';
+        alphabet[11] = 'L';
+        alphabet[12] = 'M';
+        alphabet[13] = 'N';
+        alphabet[14] = 'O';
+        alphabet[15] = 'P';
+        alphabet[16] = 'Q';
+        alphabet[17] = 'R';
+        alphabet[18] = 'S';
+        alphabet[19] = 'T';
+        alphabet[20] = 'U';
+        alphabet[21] = 'V';
+        alphabet[22] = 'W';
+        alphabet[23] = 'X';
+        alphabet[24] = 'Y';
+        alphabet[25] = 'Z';
+        // make every character in input uppercase
+        input = input.toUpperCase();
+        // for each character in the input string
+        for (int i = 0; i < input.length(); i++)
+        {
+            char thisVal = input.charAt(i);
+            if (thisVal == ' ')
+            {
+                output += ' ';
+            }
+            else
+            {
+                for (int j = 0; j < alphabetSize; j++)
+                {
+                    if (alphabet[j] == thisVal)
+                    {
+                        int shiftedIndex = (j - shift + 26) % alphabetSize;
+                        output += alphabet[shiftedIndex];
+                    }
+                }
+            }
+        }
+        return output;
+    }
+}//end of class
 
-}
+
